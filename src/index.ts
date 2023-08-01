5155b6a77186a8ac20fb410af964da783831d98a
LautaroBarrozo <106399206+LautaroBarrozo@users.noreply.github.com>
2023-08-01 14:36:01 -0300

    new cors origin

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4000
 
 const app = express()
 
-app.use(cors({origin: "https://the-sportster-space-frontend.vercel.app/", credentials: true}))
+app.use(cors({origin: "https://the-sportster-space-frontend.vercel.app", credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
