8758491c88c084f9f05be10306f5ee47a3ddff9f
LautaroBarrozo <lautaro-barrozo@outlook.com>
2023-08-01 10:43:18 -0300

    token auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -85,24 +85,18 @@ export const verifyToken = async (req: express.Request, res: express.Response) =
     const {token} = req.body
 
     if (!token) {
-        // return res.send(false)
-        const message = "!token"
-        return res.send(message)
+        return res.send(false)
     }
 
     jwt.verify(token, TOKEN_SECRET, async (err : any, user : any) => {
         if (err) {
-            const message = "err"
-            // return res.send(false)
-            return res.send(message)
+            return res.send(false)
         }
 
         const userFound = await User.findById(user.id)
 
         if (!userFound) {
-            const message = "!userfound"
-            return res.send(message)
-            // return res.send(false)
+            return res.send(false)
         }
 
 
