707cec7517578d77582b08c82b4cd06299140e03
LautaroBarrozo <lautaro-barrozo@outlook.com>
2023-08-01 09:40:32 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4000
 
 const app = express()
 
-app.use(cors({origin: "http://localhost:3000",credentials: true}))
+app.use(cors({origin: "http://localhost:3000", credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
